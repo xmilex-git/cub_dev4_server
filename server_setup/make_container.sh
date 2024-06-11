3c7dde244a3f246a19ed77bb2fb1ade1976cf57a
xmilex-git <xmilex@naver.com>
2024-06-11 14:57:17 +0900

    remove conflict env

--- server_setup/make_container.sh
+++ server_setup/make_container.sh
@@ -2,29 +2,29 @@
 
 # 인자 개수 확인
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 IP_D_CLASS host_name"
+  echo "Usage: $0 IP_D_CLASS HOST_NAME_T"
   exit 1
 fi
 
 # 변수 할당
 IP_D_CLASS=$1
-HOST_NAME=$2
+HOST_NAME_T=$2
 
-CONTAINER_NAME=${HOST_NAME}-${IP_D_CLASS}
+CONTAINER_NAME_T=${HOST_NAME_T}-${IP_D_CLASS}
 IP="192.168.226.${IP_D_CLASS}"
 
-UID=${IP_D_CLASS}0001
-GID=${IP_D_CLASS}0000
+UID_T=${IP_D_CLASS}0001
+GID_T=${IP_D_CLASS}0000
 
-GROUPNAME=${CONTAINER_NAME}_group
-USERNAME=${CONTAINER_NAME}_user
+GROUPNAME_T=${CONTAINER_NAME_T}_group
+USERNAME_T=${CONTAINER_NAME_T}_user
 
-if cat /etc/passwd | grep $USERNAME; then 
-  echo "The user ${USERNAME} already exists in /etc/passwd."
+if cat /etc/passwd | grep $USERNAME_T; then 
+  echo "The user ${USERNAME_T} already exists in /etc/passwd."
   exit 0
 else 
-  groupadd -g ${GID} ${GROUPNAME}
-  useradd -u ${UID} -g ${GROUPNAME} ${USERNAME}
+  groupadd -g ${GID_T} ${GROUPNAME_T}
+  useradd -u ${UID_T} -g ${GROUPNAME_T} ${USERNAME_T}
 fi
 
 # /root/ip.txt에서 특정 IP 주소 검색
@@ -35,12 +35,12 @@ else
   echo "No existing IP address found. Continuing..."
 fi
 
-echo "${IP}     ${HOSTNAME}" >> /root/ip.txt
+echo "${IP}     ${HOST_NAME_T}" >> /root/ip.txt
 
-mkdir   -p /data/${CONTAINER_NAME}
+mkdir   -p /data/${CONTAINER_NAME_T}
 
-podman  run -d --name ${CONTAINER_NAME} \
-	--hostname ${HOST_NAME} \
+podman  run -d --name ${CONTAINER_NAME_T} \
+	--hostname ${HOST_NAME_T} \
         --network=ipv1 \
         --ip=${IP} \
         --cap-add AUDIT_READ \
@@ -49,8 +49,10 @@ podman  run -d --name ${CONTAINER_NAME} \
         --cap-add SYS_PTRACE \
         --cap-add DAC_OVERRIDE \
         --security-opt seccomp=unconfined \
-        dev4_image_base:1 /usr/sbin/init
+        dev4_image_base:1.1 /usr/sbin/init
 
 sleep   5s
 
-podman  exec -it ${CONTAINER_NAME} /bin/bash /etc/dev4_skeleton/entrypoint.sh cubrid ${UID} ${CONTAINER_NAME}_group ${GID}
+podman  exec -it ${CONTAINER_NAME_T} /bin/bash /etc/dev4_skeleton/entrypoint.sh cubrid ${UID_T} ${CONTAINER_NAME_T}_group ${GID_T}
+
+exit    0
