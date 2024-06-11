2bdcf9e13b75a8496c38a9d2bbf3474e8919e9a8
xmilex-git <xmilex@naver.com>
2024-06-11 17:37:44 +0900

    show ip.txt

--- server_setup/make_container.sh
+++ server_setup/make_container.sh
@@ -31,6 +31,7 @@ fi
 # /root/ip.txt에서 특정 IP 주소 검색
 if grep ${IP} /root/ip.txt; then
   echo "The IP address ${IP} already exists in /root/ip.txt."
+  cat /root/ip.txt
   exit 0
 else
   echo "No existing IP address found. Continuing..."
